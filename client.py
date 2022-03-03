cbf68c461ed89d9f1e5282a8f3fd58c083b9b01f
Lena <lena.hicksonlong@gmail.com>
2022-03-03 14:08:52 -0800

    update client as example for MQTT code

--- client.py
+++ client.py
@@ -1,15 +1,48 @@
 import paho.mqtt.client as mqtt
+import json
+
+# CLIENT_ID will be defined globally, each 
+# physical Pi will have its own CLIENT_ID
+CLIEND_ID = "testid"
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("topic")
+
+    # [CLIENT_ID]/vend will tell the Pi when to vend an item
+    client.subscribe(CLIEND_ID + "/vend")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    # The payload is in JSON
+    payload = json.loads(msg.payload)
+
+    # Examples of how to get the data/what it looks like
+    order_id = payload['orderID']
+    print("order_id: " + order_id)
+
+    order_list = payload['orderList']
+    print("order_list: " + str(order_list))
+
+    first_item_id = list(order_list.keys())[0]
+    print("first_item_id: " + first_item_id)
+
+    first_item_quantity = list(order_list.values())[0]
+    print("first_item_quantity: " + str(first_item_quantity))
+
+    # Once vending is complete, construct the response object
+    # itemsDispensed MIGHT now be the original order list,
+    # if some items failed to dispense
+    response = {
+        'orderID': order_id,
+        'success': True,
+        'itemsDispensed': order_list
+    }
+    
+    # [CLIENT_ID]/confirm is for confirming orders
+    client.publish(CLIEND_ID + "/confirm", json.dumps(response))
 
 client = mqtt.Client()
 client.username_pw_set("lenatest", "password")
