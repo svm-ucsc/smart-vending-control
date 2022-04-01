ae11f16fc7a986ef1b15fa4612d6dce8edda1f00
Hannah McDonald <hmmcdona@ucsc.edu>
2022-04-01 15:39:37 -0700

    Created basic_tests().

--- weight_sensing_test.py
+++ weight_sensing_test.py
@@ -1,12 +1,25 @@
 from weight_sensor import *
-GPIO.cleanup()
-TOL = 0 #minimum squared difference for change to be registered
 
-my_sensor = WeightSensor_HX711(dout=17, pd_sck=18, gain=128)
-my_sensor.calibrate()
+def basic_tests(num_trials:int):
+    """
+    Performs basic functionality tests of weight sensors. Prints incremental progress.
+    """
+    total_error = 0
+    print("---------- Now starting basic functionality tests ----------")
+    for i in range(num_trials):
+        item_weight = input("Place an item on the scale. Enter the item's known weight in grams")
+        mes_weight = my_sensor.get_grams()
+        print("Measured weight(grams): {}".format(mes_weight))
+        dif = mes_weight - item_weight
+        total_error += dif
+        print("Difference between actual and measured weights: {}".format(dif))
+    print("Basic functionality tests complete. Average measurement error(grams): {}".format(total_error/num_trials))
 
-print("---------- Now starting basic functionality tests ----------")
-item1_weight = input("Place an item on the scale. Enter the item's weight in grams")
-print("Measured weight(grams): {}".format(my_sensor.get_grams()))
+def main():
+    my_sensor = WeightSensor_HX711(dout=17, pd_sck=18, gain=128)
+    my_sensor.calibrate()
+    basic_tests(3)
+    GPIO.cleanup()
 
-GPIO.cleanup()
+if __name__ == "__main__":
+    main()
