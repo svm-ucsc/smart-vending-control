dd15100582b5cb1a5566dc89ffbac2d04b3ca82f
Daniel Zuniga <dzuniga1117@gmail.com>
2022-04-04 19:56:38 +0100

    Removed references to rotate_pair and rotate_trio

--- movement/lane_stepper/example.py
+++ movement/lane_stepper/example.py
@@ -10,20 +10,13 @@ sys.rotate_n([0, 1], ["cw", "ccw"], [1.0, 1.0], [1.0, 1.0])
 print("Rotating 3 (ch 3/4/5) together with rotate_n")
 sys.rotate_n([3, 4, 5], ["cw", "cw", "cw"], [1.0, 1.0, 1.0], [1.0, 1.0, 1.0])
 
-# Example call to rotate_pair by manually inserting the arguments (which gets long pretty quickly)
-# sys.rotate_pair(0, "cw", 1.0, 1.0, 1, "ccw", 1.0, 1.0)
+chans = [0, 1, 2]
+dirs = ["cw", "ccw", "cw"]
+spds = [1.0, 1.0, 1.0]
+rots = [1.0, 1.0, 1.0]
 
-# Example call to rotate_trio using the unpacking feature of python's lists--could be used to 
-# perform calls to multiple lanes in the same level
-motor0_args = [0, "cw", 1.0, 1.0]
-motor1_args = [1, "ccw", 1.0, 1.0]
-motor2_args = [2, "cw", 1.0, 1.0]
-
-sys.rotate_trio(*motor0_args, *motor1_args, *motor2_args)
+print("Rtoating 3 (ch 0/1/2) together with rotate_n")
+sys.rotate_n(chans, dirs, spds, rots)
 
 sys.zero_all_pins()
 
-"""
-for i in range(0,6):
-    sys.rotate(i, "cw", 1.0, 0.5)
-"""
