2235f60e67a8ac5c757b3d8d1000a884f9035a19
Hannah McDonald <hmmcdona@ucsc.edu>
2022-04-07 12:32:13 -0700

    modified function definition

--- weight_sensing_test.py
+++ weight_sensing_test.py
@@ -1,6 +1,6 @@
 from weight_sensor import *
 
-def basic_tests(num_trials:int):
+def basic_tests(num_trials:int, sensor):
     """
     Performs basic functionality tests of weight sensors. Prints incremental progress.
     """
@@ -8,7 +8,7 @@ def basic_tests(num_trials:int):
     print("---------- Now starting basic functionality tests ----------")
     for i in range(num_trials):
         item_weight = input("Place an item on the scale. Enter the item's known weight in grams")
-        mes_weight = my_sensor.get_grams()
+        mes_weight = sensor.get_grams()
         print("Measured weight(grams): {}".format(mes_weight))
         dif = mes_weight - item_weight
         total_error += dif
@@ -18,7 +18,7 @@ def basic_tests(num_trials:int):
 def main():
     my_sensor = WeightSensor_HX711(dout=17, pd_sck=18, gain=128)
     my_sensor.calibrate()
-    basic_tests(3)
+    basic_tests(3, my_sensor)
     GPIO.cleanup()
 
 if __name__ == "__main__":
