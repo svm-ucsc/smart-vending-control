1041b95614b4aba8ccd299965ac800ee33a25241
Hannah McDonald <hmmcdona@ucsc.edu>
2022-05-05 13:49:51 -0700

    Added calib_weight_vs_accuracy() and test_round_same_item() to test script. Modified weight sensor detect change function and added reset function.

--- weight_sensing_test.py
+++ weight_sensing_test.py
@@ -1,11 +1,11 @@
 from weight_sensor import *
 
-def test_round(num_trials:int, sensor):
+def test_round_dif_items(num_trials:int, sensor):
     total_error = 0
     total_error_percent = 0
     for i in range(num_trials):
         item_weight = float(input("Place an item on the scale. Enter the item's known weight in grams\n>"))
-        mes_weight = sensor.get_grams(num_samples=1)
+        mes_weight = sensor.get_grams(num_samples=16)
         print("Measured weight(grams): {}".format(mes_weight))
         dif = (mes_weight - item_weight) if (mes_weight > item_weight) else (item_weight - mes_weight)
         dif_percent = (dif/item_weight) * 100
@@ -14,18 +14,55 @@ def test_round(num_trials:int, sensor):
         print("Difference between actual and measured weights: {:.3f}, {:.3f}%".format(dif, dif_percent))
     return (total_error, total_error_percent)
 
+def test_round_same_item(num_trials:int, sensor):
+    total_error = 0
+    total_error_percent = 0
+    print("Now starting a round of tests for one item")
+    item_weight = input("Please enter the expected weight of the item in grams: \n>")
+    for i in range(num_trials):
+        mes_weight = sensor.get_grams(num_samples=16)
+        print("Measured weight(grams): {}".format(mes_weight))
+        dif = (mes_weight - item_weight) if (mes_weight > item_weight) else (item_weight - mes_weight)
+        dif_percent = (dif/item_weight) * 100
+        total_error_percent += dif_percent
+        total_error += dif
+        print("Difference between actual and measured weights: {:.3f}, {:.3f}%".format(dif, dif_percent))
+
 def basic_tests(num_trials:int, sensor):
     """
     Performs basic functionality tests of weight sensors. Prints incremental progress.
     """
     sensor.calibrate()
     print("---------- Now starting basic functionality tests ----------")
-    total_error, total_error_percent = test_round(num_trials, sensor)
+    print("The purpose of these tests is to ensure the sensors are working as expected.")
+    print("You will weigh a few items. If the sensors are working properly, the measured weights")
+    print("Should be relatively close to the known weight")
+    total_error, total_error_percent = test_round_dif_items(num_trials, sensor)
     print("Basic functionality tests complete.")
     print("\tAverage measurement error(grams): {:.3f}".format(total_error/num_trials))
     print("\tAverage measurement error(percent): {:.3f}%".format(total_error_percent/num_trials))
 
-def location_tests(num_trials:int, sensor):
+def calib_weight_vs_accuracy(num_items, num_to_avg, sensor):
+    print("---------- Now Testing the Effect of Calibrating Weight on Measurement Accuracy ----------")
+    
+    def trial():
+        print("Now starting calibration")
+        sensor.calibrate()
+        print("For each item, including the calibrating item, you will weigh the item {} times".format(num_to_avg))
+        print("and get the average measurement accuracy.")
+        for i in range(num_items):
+            test_round_same_item(num_to_avg, sensor)
+
+    for i in range(num_items):
+        trial()
+
+def item_weight_vs_accuracy(num_items:int, sensor):
+    print("---------- Now Testing the Effect of Item Weight on Measurement Accuracy ----------")  
+
+def drop_height_test(sensor):
+    print("---------- Now Testing the Effect of Drop Height on Measurement Accuracy ----------")  
+
+def calibration_location_tests(num_trials:int, sensor):
     print("---------- Now starting location tests ----------")
     print("Instructions:\n")
     print("\tThe purpose of these tests is to test the effect of item location during calibration.")
@@ -64,11 +101,14 @@ def location_tests(num_trials:int, sensor):
     
 
 def main():
-    my_sensor = WeightSensor_HX711(dout=17, pd_sck=18, gain=128)
-    basic_tests(3, my_sensor)
-   # my_sensor.warmup()
-    #location_tests(3, my_sensor)
-    GPIO.cleanup()
+    try:
+      my_sensor = WeightSensor_HX711(dout=17, pd_sck=18, gain=128)
+      basic_tests(3, my_sensor)
+      my_sensor.reset()
+      calibration_location_tests(3, my_sensor)
+      GPIO.cleanup()
+    except:
+      GPIO.cleanup()
 
 if __name__ == "__main__":
     main()
